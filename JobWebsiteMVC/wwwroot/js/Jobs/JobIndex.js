b927351229492275d8fa8e9cacb416161abd5da6
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2022-09-28 08:54:50 +0100

    Added row click to jobs table to redirect to details

--- JobWebsiteMVC/wwwroot/js/Jobs/JobIndex.js
+++ JobWebsiteMVC/wwwroot/js/Jobs/JobIndex.js
@@ -5,4 +5,12 @@
     });
 
 
+    $('#jobsTable tr').click(function () {
+        var id = $(this).attr('id');
+        console.log("Row Id clicked: " + id);
+
+        var url = window.location.href = '/Jobs/details/' + id; //relative to domain
+        $(location).attr('href', url);
+    });
+
 });
